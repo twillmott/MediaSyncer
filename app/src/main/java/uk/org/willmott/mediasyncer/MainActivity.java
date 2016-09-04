6d5ce2b5d3db3db21a40cbb93ef406ee51236abf
Tom Willmott <tom@willmott.org.uk>
2016-09-04 15:18:10 +0100

    Added trakt authorisation. We still need to implement the saving and loading of access tokens, instead of getting a new token every time the app starts.
    Also added some simple stuff to get shows and display them.

--- app/src/main/java/uk/org/willmott/mediasyncer/MainActivity.java
+++ app/src/main/java/uk/org/willmott/mediasyncer/MainActivity.java
@@ -1,12 +1,15 @@
 package uk.org.willmott.mediasyncer;
 
-import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import com.uwetrottmann.trakt5.entities.BaseShow;
+
+import java.util.List;
 
 import uk.org.willmott.mediasyncer.service.TraktService;
 
@@ -19,28 +22,45 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        traktService = new TraktService();
-        String token = traktService.getAuthenticationStatus(getIntent());
+        // ==== Trakt Authorisation =====
+        // On start up we need to check that we have the required trakt authorisation. If we don't,
+        // we direct the user to a browser, then return to this main screen, meaning the intent will,
+        // have the redirect url attached. We need to check that if this redirect URL exists, if it
+        // does, we need to continue with the authorisation. Otherwise we need to check if we have
+        // authorisation.
+        Uri intentUri = getIntent().getData();
+        if (intentUri != null && intentUri.toString().startsWith(TraktService.REDIRECT_URL) && intentUri.getQueryParameter("code") != null) {
+            // Continue the authorisation process
+            traktService = new TraktService(intentUri.getQueryParameter("code"));
+        } else {
+            // Check for authorisation
+            traktService = new TraktService(this);
+        }
+        // ==============================
 
         Button button = (Button) findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                traktService.authenticate(MainActivity.this);
+
+                // Everything in this class is just testing (messing around).
+
+                List<BaseShow> shows;
+                List<BaseShow> showsProgress;
+                shows = traktService.getWatchedShows();
+                showsProgress = traktService.getShowWatchedProgress(shows);
+
+                TextView text = (TextView) findViewById(R.id.tempTextBox);
+                int i = 0;
+                for (BaseShow show : shows) {
+                    text.append("\n" + show.show.title);
+                    if (showsProgress.get(i).next_episode != null) {
+                        text.append(" " + showsProgress.get(i).next_episode.title);
+                    }
+                    i++;
+                }
             }
         });
-
-    }
-
-    @Override
-    protected void onNewIntent(Intent intent) {
-        Uri uri = intent.getData();
-
-        if (uri != null && uri.toString()
-                .startsWith("mediasyncer://oauthresponse")) {
-            String code = uri.getQueryParameter("code");
-            TextView textView = (TextView) findViewById(R.id.tempTextBox);
-            textView.setText(code);
-        }
     }
 }
+
--- app/src/main/java/uk/org/willmott/mediasyncer/service/TraktService.java
+++ app/src/main/java/uk/org/willmott/mediasyncer/service/TraktService.java
@@ -1,50 +1,209 @@
 package uk.org.willmott.mediasyncer.service;
 
-import android.app.IntentService;
-import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
-import android.os.IBinder;
+import android.os.AsyncTask;
 import android.util.Log;
+import com.uwetrottmann.trakt5.TraktV2;
+import com.uwetrottmann.trakt5.entities.AccessToken;
+import com.uwetrottmann.trakt5.entities.BaseShow;
+import com.uwetrottmann.trakt5.entities.Username;
+import com.uwetrottmann.trakt5.enums.Extended;
 
-import net.smartam.leeloo.client.request.OAuthClientRequest;
+import org.apache.oltu.oauth2.client.request.OAuthClientRequest;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Response;
+
+/**
+ * This is my version of an service for interfacing with Trakt. It is very heavily dependent on
+ * UweTrottmann's helper.
+ *
+ * In order to instanciate this service, you either need to supply a context, to allow the service
+ * to authenticate, or supply the code to continue to authenticate. Either method checks for
+ * authentication first.
+ */
 public class TraktService {
 
-    private static String USERID = "twillmott";
     private static String CLIENT_ID = "0866b3b94e7873b57c5fb201291adb4ba601ab4a403bd3546ff6f38cf0a8fb84";
-    private static String CLIENT_SECRED = "767159b9d53fe00d40b8e16e5172a785806b7e909570bf0360eade075d4d0a35";
-    private Context context;
+    private static String CLIENT_SECRET = "767159b9d53fe00d40b8e16e5172a785806b7e909570bf0360eade075d4d0a35";
+    private static String LOG_TAG = TraktService.class.getSimpleName();
+    public static String REDIRECT_URL = "mediasyncer://oauthresponse";
+
+    private TraktV2 trakt = new TraktV2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URL);
+    private String authenticationState;
+
+
+    public TraktService(Context context) {
+        checkAuthentication(context, null);
+    }
+
+    public TraktService(String code)  {
+        checkAuthentication(null, code);
+    }
+
+    public TraktV2 getTrakt() {
+        return trakt;
+    }
+
     /**
-     * Authenticate the trakt connection using OAuth2. This method requires a context (activity).
-     * The activity must have a intent filter to filter the redirect uri mediasyncer://oauthresponse.
-     * @param context
+     * This service checks that we have authentication. If we don't have it, we get it and save it.
+     * This method must always be called on app start up, and is called in the constructor of this
+     * service.
+     * @param context If the context is passed in, it means we're looking to create a new authorization.
      */
-    public void authenticate(Context context) {
-        this.context = context;
-
+    public void checkAuthentication(Context context, String code) {
         try {
-            String surl = "https://trakt.tv/oauth/authorize?response_type=code&client_id="
-                    + CLIENT_ID + "&redirect_uri=mediasyncer://oauthresponse";
 
-            Intent intent = new Intent(Intent.ACTION_VIEW,
-                    Uri.parse(surl));
-            context.startActivity(intent);
+            // TODO, save access tokens to database.
+
+            // First we'll try refreshing the token that we may have.
+            Response<AccessToken> accessToken = new RefreshTraktToken(trakt).execute().get();
+
+            // If this doesn't work, we need to reauthorise.
+            if (context != null && code == null) {
+                // Start by getting a new code for this new authentication.
+                OAuthClientRequest request = trakt.buildAuthorizationRequest("");
+
+                Intent intent = new Intent(Intent.ACTION_VIEW,
+                        Uri.parse(request.getLocationUri()));
+                context.startActivity(intent);
+            } else {
+                // Then get a new token from the code, this will be if we're continuing the authentication.
+                accessToken = new RetrieveTraktToken(code, trakt).execute().get();
+                trakt.accessToken(accessToken.body().access_token);
+                trakt.refreshToken(accessToken.body().refresh_token);
+            }
         } catch (Exception e) {
-            Log.e("trakt", e.toString());
+            Log.e(LOG_TAG, e.getMessage());
         }
     }
 
-    public String getAuthenticationStatus(Intent intent) {
-        Uri uri = intent.getData();
 
-        if (uri != null && uri.toString()
-                .startsWith("mediasyncer://oauthresponse"))
-        {
-            String code = uri.getQueryParameter("code");
-            // ...
+    /**
+     * Get all the watched shows for the authorised user.
+     */
+    public List<BaseShow> getWatchedShows() {
+        try {
+            return new RetrieveShows(trakt).execute().get();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
         }
-        return null;
+    }
+
+
+    /**
+     * Get the watched progress for the input show(s).
+     */
+    public List<BaseShow> getShowWatchedProgress(List<BaseShow> shows) {
+
+        List<String> showIds = new ArrayList<>();
+        for (BaseShow show : shows) {
+            showIds.add(show.show.ids.trakt.toString());
+        }
+        try {
+            return new RetrieveWatchedProgress(trakt).execute(showIds.toArray(new String[shows.size()])).get();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+
+    private class RetrieveShows extends AsyncTask<Void, Void, List<BaseShow>> {
+
+        TraktV2 trakt;
+
+        RetrieveShows(TraktV2 trakt) {
+            this.trakt = trakt;
+        }
+
+        @Override
+        protected List<BaseShow> doInBackground(Void... voids) {
+            try {
+                Response<List<BaseShow>> response = trakt.users().watchedShows(Username.ME, Extended.FULL).execute();
+                if (response.isSuccessful()) {
+                    return response.body();
+                }
+                return null;
+            } catch (IOException e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+    }
+
+
+    private class RetrieveWatchedProgress extends AsyncTask<String, Void, List<BaseShow>> {
+
+        TraktV2 trakt;
+
+        RetrieveWatchedProgress(TraktV2 trakt) {
+            this.trakt = trakt;
+        }
+
+        @Override
+        protected List<BaseShow> doInBackground(String... showIds) {
+            List<BaseShow> shows = new ArrayList<>();
+            try {
+                for (String showId : showIds) {
+                    Response<BaseShow> response = trakt.shows().watchedProgress(showId, false, false, Extended.FULL).execute();
+                    if (response.isSuccessful()) {
+                        shows.add(response.body());
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return shows;
+        }
+    }
+
+
+    private class RetrieveTraktToken extends AsyncTask<Void, Void, Response<AccessToken>> {
+
+        private String code;
+        private TraktV2 traktV2;
+
+        public RetrieveTraktToken(String code, TraktV2 traktV2) {
+            this.code = code;
+            this.traktV2 = traktV2;
+        }
+
+        @Override
+        protected Response<AccessToken> doInBackground(Void... voids) {
+            try {
+                return traktV2.exchangeCodeForAccessToken(code);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+    }
+
+
+    private class RefreshTraktToken extends AsyncTask<Void, Void, Response<AccessToken>> {
+
+        TraktV2 trakt;
+
+        RefreshTraktToken(TraktV2 trakt) {
+            this.trakt = trakt;
+        }
+
+
+        @Override
+        protected Response<AccessToken> doInBackground(Void... voids) {
+            try {
+                return trakt.refreshAccessToken();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
     }
 }
